1fb427a1df4a7cea96f01c7d229b66a805b51660
Tommi Hagelberg <tommihagelberg@gmail.com>
2017-01-24 15:54:13 +0200

    Started working on camera movement.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Diipa daapa
+/// </summary>
+public class Camera : MonoBehaviour
+{
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
