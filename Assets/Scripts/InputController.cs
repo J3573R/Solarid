54cd0ea55e62f4031a5edcae9977146df243478e
Tommi Hagelberg <tommihagelberg@gmail.com>
2017-01-30 17:54:44 +0200

    Refactoring input controller.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -2,8 +2,16 @@
 
 public class InputController : MonoBehaviour
 {
-
+    // Speed of the player
+    public float Speed = 5f;
+    // Rotation speed of the player
+    public float RotationSpeed = 8f;
+    
     private Rigidbody _rigidbody;
+    private Vector3 _vMousePos;
+    private float _fAngle;
+    private float _fHDir;
+    private float _fVDir;
 
     void Awake()
     {
@@ -12,30 +20,40 @@ public class InputController : MonoBehaviour
 
     void Update()
     {
-        Vector3 playerPosOnScreen = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 mousePos = Input.mousePosition;
-
-        mousePos.x -= Screen.width / 2;
-        mousePos.y -= Screen.height / 2;
-
-        mousePos += transform.position;
-
-        float angle = Vector3.Angle(-mousePos, Vector3.up);
-        //float angle = Vector3.Angle(playerPosOnScreen, -mousePos );
-
-        if (mousePos.x > 0)
-            angle = 360 - angle;
-
-        transform.rotation = Quaternion.Euler(0, angle + 180, 0);
-
-  
-        var horizontalDirection = Input.GetAxis("Horizontal");
-        var verticalDirection = Input.GetAxis("Vertical");
-        //transform.Translate(horizontalDirection * 10f *Time.deltaTime, 0f, verticalDirection * 10f * Time.deltaTime);
-        _rigidbody.velocity = new Vector3(horizontalDirection * 5f, 0f, verticalDirection * 5f);
- 
+        ListenMouse();
+        Move();
     }
-   
+
+    /// <summary>
+    /// Defines mouse position from screen size and turns the players Y axis the right amount.
+    /// </summary>
+    private void ListenMouse()
+    {
+        _vMousePos = Input.mousePosition;
+
+        _vMousePos.x -= (float)Screen.width / 2;
+        _vMousePos.y -= (float)Screen.height / 2;
+
+        _vMousePos += transform.position;
+
+        _fAngle = Vector3.Angle(_vMousePos, Vector3.up);
+
+        if (_vMousePos.x < 0)
+            _fAngle = 360 - _fAngle;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, _fAngle, 0), Time.deltaTime * RotationSpeed);
+    }
+
+    /// <summary>
+    /// Listens keyboard input and increases horizontal & vertical velocity of player times Speed.
+    /// </summary>
+    private void Move()
+    {
+        _fHDir = Input.GetAxis("Horizontal");
+        _fVDir = Input.GetAxis("Vertical");
+        _rigidbody.velocity = new Vector3(_fHDir * Speed, 0f, _fVDir * Speed);
+    }
+
 }
 
 
