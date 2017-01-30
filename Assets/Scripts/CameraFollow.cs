5ef35cc6ea5cd42b9d2bdee2c8b8d34806591f8f
Tommi Hagelberg <tommihagelberg@gmail.com>
2017-01-30 17:16:15 +0200

    Camera settings.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,24 @@
 
 public class CameraFollow : MonoBehaviour
 {
+    // Speed of camera delay when following player
+    public float CameraDelay = 5f;
+    // Camera offset from player
+    public Vector3 CameraOffset = new Vector3(0, 10, -5);
+
+    // Players gameobject
     private GameObject _player;
+    // Cameras current position
+    private Vector3 _vCurPos; 
 
     void Awake()
     {
         _player = GameObject.Find("Player");
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        //transform.position = new Vector3(_player.transform.position.x, 10f, _player.transform.position.z - 5);
+        _vCurPos = _player.transform.position + CameraOffset;
+        transform.position = Vector3.Lerp(transform.position, _vCurPos, Time.smoothDeltaTime * CameraDelay);
     }
 }
