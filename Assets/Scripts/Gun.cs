67edf7c741619d168fe24e87dbd180b4b41f58eb
Tommi Hagelberg <tommihagelberg@gmail.com>
2017-01-30 18:52:39 +0200

    Firerate added.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-
+    // Bullet prefab
     public GameObject Bullet;
+    // Reload time of the gun in seconds
+    public float Reload;
 
+    // Guns collider location
     private Collider _collider;
+    // Current status of reload
+    private float _reload = 0;
 
     void Awake()
     {
         _collider = GetComponent<Collider>();
     }
 
+    void Update()
+    {
+        if (_reload > 0)
+        {
+            _reload -= Time.deltaTime * 1;
+        }
+        Debug.Log(_reload);
+    }
+
     public void Shoot()
     {
-        GameObject bullet = Instantiate(Bullet, _collider.transform.position, transform.rotation);
-        
+        if (_reload <= 0)
+        {
+            Instantiate(Bullet, _collider.transform.position, transform.rotation);
+            _reload = Reload;
+        }
     }
 }
