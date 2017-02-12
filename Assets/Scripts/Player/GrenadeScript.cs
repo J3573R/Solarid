37efaf0a1f503506d14cde471eb16d4d209ec3ef
Teemu Arponen <teemu.arponen@cs.tamk.fi>
2017-02-12 10:29:39 +0200

    Fiddling with the grenade

--- Assets/Scripts/Player/GrenadeScript.cs
+++ Assets/Scripts/Player/GrenadeScript.cs
@@ -21,6 +21,7 @@ public class GrenadeScript : MonoBehaviour {
         _renderer = GetComponent<MeshRenderer>();
         _rigidBody = GetComponent<Rigidbody>();
         _rigidBody.useGravity = false;
+        _rigidBody.velocity = Vector3.zero;
         _renderer.enabled = false;
         _explosionParticle = Instantiate(_explosionParticle, transform.position, Quaternion.identity);
         _explosionParticle.Stop();
@@ -40,12 +41,12 @@ public class GrenadeScript : MonoBehaviour {
     public void Throw()
     {
 
-        _renderer.enabled = true;
-        _rigidBody.useGravity = true;
+        //_renderer.enabled = true;
+        //_rigidBody.useGravity = true;
         startPosition = transform.position;
         CalculatePositions();
 
-        _rigidBody.velocity = BallisticVelocity(targetPosition, angle);
+        //_rigidBody.velocity = BallisticVelocity(targetPosition, angle);
     }
 
     private void Move()
@@ -61,9 +62,8 @@ public class GrenadeScript : MonoBehaviour {
         float distance = Vector3.Distance(startPosition, targetPosition);
 
         Debug.Log("DISTANCE = " + distance);
+        Debug.Log("MINDISTANCE = " + minDistance);
         Vector3 direction = heading.normalized;
-
-        Debug.Log(targetPosition);
         
 
         if (distance < minDistance)
@@ -78,12 +78,10 @@ public class GrenadeScript : MonoBehaviour {
             targetPosition = startPosition + (direction * distance);
         }
 
-        
+        Debug.Log("NEWDISTANCE = " + distance);
 
-        Debug.Log(targetPosition);
-
-
-
+        _explosionParticle.transform.position = targetPosition;
+        _explosionParticle.Play();
     }
 
     Vector3 BallisticVelocity(Vector3 target, float angle)
@@ -93,7 +91,7 @@ public class GrenadeScript : MonoBehaviour {
         dir.y = 0; // retain only the horizontal difference
         float dist = dir.magnitude + 0.2f; // get horizontal direction
         float a = angle * Mathf.Deg2Rad; // Convert angle to radians
-        Debug.Log(Mathf.Tan(a));
+
         dir.y = dist * Mathf.Tan(a); // set dir to the elevation angle.
         dist += height / Mathf.Tan(a); // Correction for small height differences
 
