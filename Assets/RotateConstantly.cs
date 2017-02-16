424ee223eff320cbb90365d7e0f5f8eda2c74944
Tommi Hagelberg <tommihagelberg@gmail.com>
2017-02-16 21:57:47 +0200

    Fog rotation hurr durr.

---
+++ Assets/RotateConstantly.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateConstantly : MonoBehaviour
+{
+
+    public Vector3 RotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(RotationSpeed * Time.deltaTime);
+    }
+}
