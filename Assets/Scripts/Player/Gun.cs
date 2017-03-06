c241d5238bf2a08e633def49e01a3098c35b50b3
Teemu Arponen <teemu.arponen@cs.tamk.fi>
2017-03-06 09:13:46 +0200

    Small fixes

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -80,7 +80,7 @@ public class Gun : MonoBehaviour
                 _recoil += RecoilBuildUp;
             }
         }
-        Debug.Log(_recoil);
+        //Debug.Log(_recoil);
     }
 
     /// <summary>
@@ -110,9 +110,10 @@ public class Gun : MonoBehaviour
     private Vector3 GetTargetPosition()
     {
         Vector3 direction = _player.transform.forward;
-        Debug.Log(direction);
+        //Debug.Log(direction);
 
         float tmp = UnityEngine.Random.Range(-_recoil, _recoil);
+        //Debug.Log(tmp);
         Vector3 vec = Quaternion.AngleAxis(tmp, Vector3.forward) * direction;
 
         return transform.position + direction * TargetDistance;
