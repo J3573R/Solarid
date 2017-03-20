debacbf6d1e0539d2756a72a669cdf5d803effbd
Tommi Hagelberg <tommihagelberg@gmail.com>
2017-03-20 17:31:59 +0200

    Anubis fidling. Enemy spawner & activators. Relocating scripts.

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Activator : MonoBehaviour
+{
+    // Objects to activate
+    public List<GameObject> ActivateObjects;
+    
+    public bool ActivateChildren;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (ActivateObjects.Count > 0)
+            {
+                foreach (var currentObject in ActivateObjects)
+                {
+                    currentObject.SetActive(true);
+                }
+            }
+
+            if (ActivateChildren)
+            {
+                Debug.Log("HERE");
+                Transform[] children = GetComponentsInChildren<Transform>();
+                foreach (Transform child in transform)
+                {
+                    Debug.Log("ACTIVATE");
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+        
+    }
+}
