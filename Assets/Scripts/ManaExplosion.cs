e95c4379b3780d2a8d8b64f9e8736a94dabd8770
WPK\c3thagel <tommihagelberg@gmail.com>
2017-04-07 11:43:52 +0300

    Mana globe in progress.

---
+++ Assets/Scripts/ManaExplosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaExplosion : MonoBehaviour {
+
+    public GameObject ManaGlobePrefab;
+    public int ManaAmount;
+    
+	void Explode()
+    {
+        for(int i = 0; i < ManaAmount; i++)
+        {
+
+        }
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ManaGlobe.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaGlobe : MonoBehaviour {
+
+    public float MaxSpeed;
+
+    private Vector3 _moveDirection;
+    private float _speed = -2;
+
+    void Awake()
+    {
+        _moveDirection = new Vector3(Random.Range(0, 100), Random.Range(0, 100), Random.Range(0, 100)).normalized;
+    }
+	
+	void FixedUpdate () {
+        if(_speed < 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + -_moveDirection * _speed, Time.deltaTime);
+        } else
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + _moveDirection * _speed, Time.deltaTime);
+        }        
+
+        if(_speed > 0)
+        {
+            _moveDirection = Globals.Player.transform.position - transform.position;
+            _moveDirection = _moveDirection.normalized;
+        }
+
+        if(_speed < MaxSpeed)
+        {
+            _speed += Time.deltaTime;
+        }
+	}
+
+    // TODO: Return me correctly to pool
+    void ReturnToPool()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ReturnToPool();
+        }
+    }
+
+}
